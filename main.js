396c41fe34de66b199816433014fdb855eb51d83
aditi419 <76972940+aditi419@users.noreply.github.com>
2021-06-21 13:10:13 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw(){
+    background("#ADD8E6");
+    textSize(difference);
+    text('Aditi', 30, 300)
+    fill("#0000FF");
+    stroke("#000000");
+}
+
+function modelLoaded(){
+    console.log("PoseNet Model Loaded")
+}
+
+function gotPoses(results){
+     if(results.length > 0){
+         console.log(results);
+
+         noseX = results[0].pose.nose.x;
+         noseY = results[0].pose.nose.y;
+         leftWristX = results[0].pose.leftWrist.x;
+         rightWristX = results[0].pose.rightWrist.x;
+         difference = floor(leftWristX - rightWristX);
+     }
+}
